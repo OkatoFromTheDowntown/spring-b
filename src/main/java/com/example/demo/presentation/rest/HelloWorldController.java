37c15b430350688595fabf934be37c8c2dff7790
stayfunkybros <stayfunkybros@mac.mini>
2022-02-21 19:05:22 +0900

    commit rest api controller

--- src/main/java/com/example/demo/presentation/rest/HelloWorldController.java
+++ src/main/java/com/example/demo/presentation/rest/HelloWorldController.java
@@ -1,6 +1,7 @@
-package com.example.demo.application;
+package com.example.demo.presentation.rest;
 
-import com.example.demo.annotation.ApiVersion;
+import com.example.demo.presentation.annotation.ApiVersion;
+import com.example.demo.presentation.exception.ApplicationException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,23 +14,28 @@ public class HelloWorldController {
     @ApiVersion("v0")
     @GetMapping("/hello")
     public String helloWorld() {
-        return "HelloWorld!";
+        return "Hello World!";
     }
 
     @ApiVersion("v1")
     @GetMapping("/hello")
     public String helloMac() {
-        return "HelloMac!";
+        return "Hello V1!";
     }
 
     @ApiVersion("v2")
     @GetMapping("/hello")
     public String helloWindows() {
-        return "HelloWindows!";
+        return "Hello V2!";
     }
 
     @GetMapping("/author")
     public String author() {
         return "Written by Okato";
+    }
+
+    @GetMapping("/test/err")
+    public String exceptions() {
+        throw new ApplicationException("Testing for exception handling from Application Controller");
     }
 }
